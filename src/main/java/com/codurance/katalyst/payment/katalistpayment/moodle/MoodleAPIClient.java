0a44998917009075349d298b81b79ebde30ab81c
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-09-11 15:12:31 +0200

    refactor MoodleAPIClient

--- src/main/java/com/codurance/katalyst/payment/katalistpayment/moodle/MoodleAPIClient.java
+++ src/main/java/com/codurance/katalyst/payment/katalistpayment/moodle/MoodleAPIClient.java
@@ -3,7 +3,10 @@ package com.codurance.katalyst.payment.katalistpayment.moodle;
 import com.codurance.katalyst.payment.katalistpayment.utils.Mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -20,6 +23,19 @@ public class MoodleAPIClient {
     public static final String WSFUNCTION = "&wsfunction=";
     public static final String MOODLEWSRESTFORMAT = "&moodlewsrestformat=";
     public static final String STUDENT_ROLE_ID = "5";
+    public static final String COURSEID = "courseid";
+    public static final String FIELD = "field";
+    public static final String VALUES_ARRAY_0 = "values[0]";
+    public static final String USERS_0_USERNAME = "users[0][username]";
+    public static final String USERS_0_CREATEPASSWORD = "users[0][createpassword]";
+    public static final String USERS_0_EMAIL = "users[0][email]";
+    public static final String USERS_0_FIRSTNAME = "users[0][firstname]";
+    public static final String USERS_0_LASTNAME = "users[0][lastname]";
+    public static final String CREATE_PASWORD_AND_SEND = "1";
+    public static final String ENROLMENTS_0_ROLEID = "enrolments[0][roleid]";
+    public static final String ENROLMENTS_0_USERID = "enrolments[0][userid]";
+    public static final String ENROLMENTS_0_COURSEID = "enrolments[0][courseid]=";
+    public static final String OPTIONS_IDS_0 = "options[ids][0]";
     @Value("${moodle.urlbase}")
     private String URL_BASE;
 
@@ -36,15 +52,17 @@ public class MoodleAPIClient {
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
         return  request;
     }
-    private String createURL(String moodleWsFunction) {
+    private String generateEndPoint(String moodleWsFunction) {
         return URL_BASE+ WSTOKEN + token + WSFUNCTION +moodleWsFunction+ MOODLEWSRESTFORMAT +format;
     }
     private List<MoodleUserDTO> getUsersForCourse(String courseId) {
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        String url = createURL("core_enrol_get_enrolled_users");
-        map.add("courseid", courseId);
+        map.add(COURSEID, courseId);
         HttpEntity<MultiValueMap<String, String>> request = createRequest(map);
-        ResponseEntity<MoodleUserDTO[]> response = restTemplate.postForEntity(url, request, MoodleUserDTO[].class);
+        ResponseEntity<MoodleUserDTO[]> response = restTemplate.postForEntity(
+                generateEndPoint("core_enrol_get_enrolled_users"),
+                request,
+                MoodleUserDTO[].class);
         return Arrays.stream(response.getBody()).toList();
     }
 
@@ -75,13 +93,15 @@ public class MoodleAPIClient {
         ResponseEntity<MoodleUserDTO[]> response = null;
         MoodleUserDTO result = null;
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        String url = createURL("core_user_get_users_by_field");
-        map.add("field", "email");
-        map.add("values[0]",  email);
+        map.add(FIELD, "email");
+        map.add(VALUES_ARRAY_0,  email);
         HttpEntity<MultiValueMap<String, String>> request = createRequest(map);
 
         try {
-            response = restTemplate.postForEntity(url, request, MoodleUserDTO[].class);
+            response = restTemplate.postForEntity(
+                    generateEndPoint("core_user_get_users_by_field"),
+                    request,
+                    MoodleUserDTO[].class);
             result = getFirst(response);
         } catch (Exception ex) {
             //TODO: Include log and Throw Exception
@@ -95,17 +115,19 @@ public class MoodleAPIClient {
         ResponseEntity<MoodleUserDTO[]> response = null;
         MoodleUserDTO result = null;
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        String url = createURL("core_user_create_users");
         Mail mail = new Mail(email);
-        map.add("users[0][username]", mail.getUserName());
-        map.add("users[0][createpassword]", "1");
-        map.add("users[0][email]",  email);
-        map.add("users[0][firstname]",name);
-        map.add("users[0][lastname]", surname);
+        map.add(USERS_0_USERNAME, mail.getUserName());
+        map.add(USERS_0_CREATEPASSWORD, CREATE_PASWORD_AND_SEND);
+        map.add(USERS_0_EMAIL,  email);
+        map.add(USERS_0_FIRSTNAME,name);
+        map.add(USERS_0_LASTNAME, surname);
         HttpEntity<MultiValueMap<String, String>> request = createRequest(map);
 
         try {
-            response = restTemplate.postForEntity(url, request, MoodleUserDTO[].class);
+            response = restTemplate.postForEntity(
+                    generateEndPoint("core_user_create_users"),
+                    request,
+                    MoodleUserDTO[].class);
             result = getFirst(response);
         } catch (Exception ex) {
             //TODO: Include log and Throw Exception
@@ -118,16 +140,18 @@ public class MoodleAPIClient {
     public void enroleToTheCourse(MoodleCourseDTO course, MoodleUserDTO user) {
         MoodleUserDTO result = null;
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        String url = createURL("enrol_manual_enrol_users");
         String roleId = STUDENT_ROLE_ID;
         String userId = user.getId();
-        map.add("enrolments[0][roleid]", roleId);
-        map.add("enrolments[0][userid]", userId);
-        map.add("enrolments[0][courseid]=", course.getId()+"");
+        map.add(ENROLMENTS_0_ROLEID, roleId);
+        map.add(ENROLMENTS_0_USERID, userId);
+        map.add(ENROLMENTS_0_COURSEID, course.getId()+"");
         HttpEntity<MultiValueMap<String, String>> request = createRequest(map);
 
         try {
-            restTemplate.postForEntity(url, request, String.class);
+            restTemplate.postForEntity(
+                    generateEndPoint("enrol_manual_enrol_users"),
+                    request,
+                    String.class);
         } catch (Exception ex) {
             //TODO: Include log and Throw Exception
             String errorMessage = ex.getMessage();
@@ -138,12 +162,14 @@ public class MoodleAPIClient {
         ResponseEntity<MoodleCourseDTO[]> response = null;
         MoodleCourseDTO result = null;
         MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-        String url = createURL("core_course_get_courses");
-        map.add("options[ids][0]", courseId);
+        map.add(OPTIONS_IDS_0, courseId);
         HttpEntity<MultiValueMap<String, String>> request = createRequest(map);
 
         try {
-            response = restTemplate.postForEntity(url, request, MoodleCourseDTO[].class);
+            response = restTemplate.postForEntity(
+                    generateEndPoint("core_course_get_courses"),
+                    request,
+                    MoodleCourseDTO[].class);
             result = getFirst(response);
         } catch (Exception ex) {
             //TODO: Include log and Throw Exception
