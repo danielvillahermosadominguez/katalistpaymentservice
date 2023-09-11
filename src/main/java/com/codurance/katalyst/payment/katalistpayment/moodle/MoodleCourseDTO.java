362f911c91b572ac9a164e9fefc15a1bd6076860
Daniel Villahermosa <daniel.villahermosa@codurance.com>
2023-09-11 15:14:00 +0200

    remove some fields from MoodleCourseDTO

--- src/main/java/com/codurance/katalyst/payment/katalistpayment/moodle/MoodleCourseDTO.java
+++ src/main/java/com/codurance/katalyst/payment/katalistpayment/moodle/MoodleCourseDTO.java
@@ -2,11 +2,7 @@ package com.codurance.katalyst.payment.katalistpayment.moodle;
 
 public class MoodleCourseDTO {
     private int id;
-    private String fullname;
     private String displayname;
-    private String shortname;
-    private int categoryid;
-
 
     public int getId() {
         return id;
